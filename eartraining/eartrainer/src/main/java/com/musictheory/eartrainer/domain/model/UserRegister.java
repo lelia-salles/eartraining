4cd9e6c3c82d8e3eac24197f6996921a0bdaa1bf
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2025-03-31 11:46:48 -0300

    update UserRegister

--- eartraining/eartrainer/src/main/java/com/musictheory/eartrainer/domain/model/UserRegister.java
+++ eartraining/eartrainer/src/main/java/com/musictheory/eartrainer/domain/model/UserRegister.java
@@ -1,60 +1,41 @@
 package com.musictheory.eartrainer.domain.model;
 
 import jakarta.persistence.*;
+import lombok.*;
+import com.musictheory.eartrainer.util.PasswordEncoderUtil;
 
 @Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id", "email"})
+@ToString(exclude = "password")
 public class UserRegister {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 50)
     private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
-    @Entity
-    public class UserRegister {
-
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-
-        private String name;
-        private String email;
-        private String password;
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.setPassword(password);
     }
 
+    public void setPassword(String password) {
+        this.password = PasswordEncoderUtil.encode(password);
+    }
+
+    public String getMaskedPassword() {
+        return "********";
+    }
 }
